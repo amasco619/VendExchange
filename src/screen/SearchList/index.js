4bf6134664a662573968bdc34c99c56a704b0053
Benedict <ama.ikegwuonu@gmail.com>
2021-03-13 01:35:19 +0100

    G-Map

--- src/screen/SearchList/index.js
+++ src/screen/SearchList/index.js
@@ -1,23 +1,43 @@
-import React, { useState } from 'react'
-import { View, TextInput, Text, FlatList, Pressable } from 'react-native'
+import React from 'react'
+import { View, Text, FlatList, Pressable } from 'react-native'
 import Search from '../../../assests/data/Search'
 import styles from './styles'
 import Entypo from 'react-native-vector-icons/Entypo'
-import {useNavigation} from '@react-navigation/native'
+import { useNavigation } from '@react-navigation/native'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import LocationRow from './LocationRow'
 
 const SearchList = () => {
 
     const navigation = useNavigation()
-    const [input, setInput] = useState("")
 
+// React Native Google Place - yarn add react-native-google-places-autocomplete
+//https://www.npmjs.com/package/react-native-google-places-autocomplete
     return (
         <View style={styles.container}>
-            {/* Input component */}
-            <TextInput 
-                style={styles.textInput}
-                placeholder="What do you seek?"
-                value={input}
-                onChangeText={setInput}/>
+            <GooglePlacesAutocomplete
+                placeholder='Choose a location'
+                onPress={(data, details = null) => {
+                    // 'details' is provided when fetchDetails = true
+                    console.log(data, details);
+                    navigation.navigate('Home', {
+                        screen: 'Search',
+                        params: {
+                            screen: 'SearchResults'
+                        }
+                    })
+                }}
+                fetchDetails
+                styles={{textInput: styles.textInput}}
+                query={{
+                    key: 'AIzaSyCuyNNQbJcq2x9nH6xNV0Rus9fUp690KJo',
+                    language: 'en',
+                    components: 'country:ng',
+                    types: '(cities)',
+                }}
+                suppressDefaultStyles
+                renderRow={(item) => <LocationRow item={item} />}
+            />
             
             {/* Search result List of Locations */}
             <FlatList 
